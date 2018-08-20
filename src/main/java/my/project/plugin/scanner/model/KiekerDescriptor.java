a7a9ada7642047b84edc7270f3a85d0e347bc4bd
DESKTOP-RBHO8OA\Richard Müller <rmueller@wifa.uni-leipzig.de>
2018-08-20 17:06:41 +0200

    update model

--- src/main/java/my/project/plugin/scanner/model/KiekerDescriptor.java
+++ src/main/java/my/project/plugin/scanner/model/KiekerDescriptor.java
@@ -2,11 +2,10 @@ package my.project.plugin.scanner.model;
 
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
-// tag::class[]
+
 /**
  * Defines the label which is shared by all nodes representing Kieker structures.
  */
-@Label("KIEKER")
+@Label("Kieker")
 public interface KiekerDescriptor extends Descriptor {
 }
-// end::class[]
