bc60be3ccd9d5a19c550b169582e1309251e0d83
Richard Müller <rmueller@wifa.uni-leipzig.de>
2019-08-12 15:17:47 +0200

    introduce name for MethodDescriptor

--- src/main/java/org/jqassistant/contrib/plugin/kieker/api/model/MethodDescriptor.java
+++ src/main/java/org/jqassistant/contrib/plugin/kieker/api/model/MethodDescriptor.java
@@ -1,5 +1,6 @@
 package org.jqassistant.contrib.plugin.kieker.api.model;
 
+import com.buschmais.jqassistant.plugin.common.api.model.NamedDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 /**
@@ -7,9 +8,10 @@ import com.buschmais.xo.neo4j.api.annotation.Label;
  * A Method is identified by its signature.
  */
 @Label("Method")
-public interface MethodDescriptor extends KiekerDescriptor {
+public interface MethodDescriptor extends NamedDescriptor, KiekerDescriptor {
 
     void setSignature(String signature);
+
     String getSignature();
 
 }
