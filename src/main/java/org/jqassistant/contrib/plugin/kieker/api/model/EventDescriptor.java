89b0aea18005969d47f34dc671dc53488738461d
Richard Müller <rmueller@wifa.uni-leipzig.de>
2019-08-22 15:30:43 +0200

    introduce execution event

---
+++ src/main/java/org/jqassistant/contrib/plugin/kieker/api/model/EventDescriptor.java
@@ -0,0 +1,11 @@
+package org.jqassistant.contrib.plugin.kieker.api.model;
+
+import com.buschmais.xo.neo4j.api.annotation.Label;
+
+/**
+ * Defines the node for an event.
+ */
+@Label("Event")
+public interface EventDescriptor extends KiekerDescriptor {
+
+}
